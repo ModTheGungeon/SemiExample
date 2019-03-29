800ccb4d3a02b8530b70f194c36b2c7791048351
Zatherz <zatherz@linux.pl>
2019-03-29 12:19:05 +0100

    Initial Commit

---
+++ SemiExample/Example.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Semi;
+using ModTheGungeon;
+
+namespace SemiExample {
+    public class Example : Mod {
+        public Logger Logger = new Logger("ExampleMod");
+
+        public override void Loaded() {
+            Logger.Info("Example mod loaded!");
+        }
+    }
+}
